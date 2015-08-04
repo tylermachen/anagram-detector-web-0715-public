4026be509304d67d3e1547e91bef40c383010ccd
Tyler Machen <tylermachen@gmail.com>
2015-08-04 15:27:45 -0400

    complete Anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagrams.select { |a| word.split('').sort == a.split('').sort }
+  end
+end
